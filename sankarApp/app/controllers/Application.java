914907f51e3fd9be059f08d331c446faaac9e08c
Sankar Rangaswamy <rangaswamy@ge.com>
2013-02-18 13:06:05 -0800

    My first test play 2.0.4 framework application. This is just a sample revision.

---
+++ sankarApp/app/controllers/Application.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import views.html.*;
+
+public class Application extends Controller {
+  
+  public static Result index() {
+
+	System.out.println("t test");
+    	return ok(index.render("Your new application is ready."));
+  }
+  
+}
